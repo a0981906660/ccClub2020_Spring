8e15e1a2355478dfd2719cf64ffb449fafa5d6a9
Andy <103405181@nccu.edu.tw>
2020-06-21 01:27:46 +0800

    0621

--- R/01 - Earthquake_0621.R
+++ R/01 - Earthquake_0621.R
@@ -1,10 +1,39 @@
 #讀檔
 setwd("/Users/Andy 1/Google 雲端硬碟 (r08323004@g.ntu.edu.tw)/0 Semesters/108-2/二 ccClub/0 Github Repo/ccClub2020_Spring/R")
 
-#Read Fama
-earthquake = read.csv("/Users/Andy 1/Google 雲端硬碟 (r08323004@g.ntu.edu.tw)/0 Semesters/108-2/二 ccClub/0 Github Repo/ccClub2020_Spring/R/raw/earthquake_data.csv")
+#Read files
+earthquake = read.csv("raw/combined_0621.csv")
+
+# Descriptive Statistics
+boxplot(earthquake$Distance)
+quantile(na.omit(earthquake$Distance), probs = c(.5,.6,.7,.8,.9,.95,.99,1))
+
+# 距離超過244就不要
+earthquake = earthquake[earthquake$Distance<=244,]
+boxplot(earthquake$Distance)
+hist(earthquake$Distance)
 
 
+# Regression
+
+# Part 1: Probit
+## We aim to know the probability of 搶到地震爆文
+#LPM
+reg1 = lm(YouWin ~ Distance, data = earthquake)
+summary(reg1)
+
+#Probit
+reg2 = glm(YouWin ~ Distance, family = binomial(link = "probit"), data = earthquake)
+summary(reg2)
+
+#Logit
+reg3 = glm(YouWin ~ Distance, family = binomial(link = "logit"), data = earthquake)
+summary(reg3)
+
+
+
+
+######################################################################
 library(glmnet) #Lasso
 
 #Def 1
